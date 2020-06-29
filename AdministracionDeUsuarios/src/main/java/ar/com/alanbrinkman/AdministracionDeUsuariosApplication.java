b05ba2addbfa3ba775d851ed13b6a6f88939ed08
Ing. Alan Brinkman <alan.brinkman@gmail.com>
2020-06-29 08:37:52 -0300

    Commit inicial

---
+++ AdministracionDeUsuarios/src/main/java/ar/com/alanbrinkman/AdministracionDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.alanbrinkman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionDeUsuariosApplication.class, args);
+	}
+
+}
---
+++ AdministracionDeUsuarios/src/test/java/ar/com/alanbrinkman/AdministracionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.alanbrinkman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
